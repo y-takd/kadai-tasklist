892c53234b985ba9740d854265a862fc75056af1
y.takd <y_takd@yahoo.co.jp>
2021-07-09 20:49:57 +0900

    create index and new servlet

--- src/models/Task.java
+++ src/models/Task.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 @NamedQueries({
     @NamedQuery(
             name= "getAllTasks",
-            query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+            query = "SELECT t FROM Task AS t ORDER BY t.id ASC"
             )
 })
 
